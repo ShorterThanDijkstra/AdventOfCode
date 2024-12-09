dcaf21d3bfffa9604c9e913c209775169712fea1
ShorterThanDijkstra <db1345569965@gmail.com>
2024-12-09 16:14:33 +0800

    day1

--- 2024/Haskell/Day1.hs
+++ 2024/Haskell/Day1.hs
@@ -1,16 +1,31 @@
 module Day1 where
 
 import Data.List (sort)
+import Data.Map (lookup, insert, empty)
 
-parseLines :: [String] -> [(Int, Int)]
-parseLines lines =
-  let (l, r) = foldr (\s (l, r) -> let is = map read (words s) in (head is : l, last is : r)) ([], []) lines
-   in zip (sort l) $ sort r
+parseLines :: [String] -> ([Int], [Int])
+parseLines = foldr (\s (l, r) -> let is = map read (words s) in (head is : l, last is : r)) ([], []) 
 
-distance :: [(Int, Int)] -> Int
-distance  = foldr (\(a, b) d -> abs (a - b) + d) 0
+distance :: ([Int], [Int]) -> Int
+distance (l, r) = foldr (\(a, b) d -> abs (a - b) + d) 0 $ zip (sort l) (sort r)
 
-main :: IO ()
-main = do
+part1 :: IO ()
+part1 = do
   lines <- lines <$> readFile "input_day1.txt"
   print (distance $ parseLines lines)
+
+similarity :: ([Int], [Int]) -> Int
+similarity (l, r) = let m = foldr (\i m -> case Data.Map.lookup i m of 
+                                             Nothing -> insert i 1 m 
+                                             Just count -> insert i (count + 1) m)
+                                  empty
+                                  r 
+                    in foldr (\i res -> case Data.Map.lookup i m of
+                                          Nothing -> res 
+                                          Just count -> i * count + res)
+                             0
+                             l
+part2 :: IO ()
+part2 = do
+  lines <- lines <$> readFile "input_day1.txt"
+  print (similarity $ parseLines lines)
